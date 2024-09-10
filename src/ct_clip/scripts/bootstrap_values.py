4b7bf76ff28012dfa65afe973ceeeed207cbd04b
John Flavin <flavinj@gmail.com>
2024-09-10 11:21:35 -0500

    Convert bootstrap_values script to proper format

--- src/ct_clip/scripts/bootstrap_values.py
+++ src/ct_clip/scripts/bootstrap_values.py
@@ -61,138 +61,116 @@ def find_threshold(probabilities, true_labels):
     return best_threshold
 
 
-# Dictionary mapping labels to diagnoses
-label_to_diagnosis = {
-    0: "Medical material",
-    1: "Arterial wall calcification",
-    2: "Cardiomegaly",
-    3: "Pericardial effusion",
-    4: "Coronary artery wall calcification",
-    5: "Hiatal hernia",
-    6: "Lymphadenopathy",
-    7: "Emphysema",
-    8: "Atelectasis",
-    9: "Lung nodule",
-    10: "Lung opacity",
-    11: "Pulmonary fibrotic sequela",
-    12: "Pleural effusion",
-    13: "Mosaic attenuation pattern",
-    14: "Peribronchial thickening",
-    15: "Consolidation",
-    16: "Bronchiectasis",
-    17: "Interlobular septal thickening",
-}
+def main(data_dir: str | Path):
+    data_dir = data_dir if isinstance(data_dir, Path) else Path(data_dir)
 
-# Path to inference output directory
-data_dir = "/path_to_inference_output/"
+    # Load predicted and labels data
+    predicted_data = np.load(data_dir / "predicted_weights.npz")
+    labels_data = np.load(data_dir / "labels_weights.npz")
 
-# Load predicted and labels data
-predicted_data = np.load(Path(data_dir) / "predicted_weights.npz")
-labels_data = np.load(Path(data_dir) / "labels_weights.npz")
+    # Extracting the arrays from the loaded files
+    labels = labels_data["data"]
+    predicted = predicted_data["data"]
 
-# Extracting the arrays from the loaded files
-labels = labels_data["data"]
-predicted = predicted_data["data"]
+    # Thresholds list
+    thresholds = []
 
-# Thresholds list
-thresholds = []
-
-# Find threshold for each label
-for i in range(18):
-    logit = predicted[:, i]
-    l = labels[:, i]
-    prob = logit
-    threshold = find_threshold(prob, l)
-    thresholds.append(threshold)
-
-# Initialize DataFrames for storing evaluation metrics
-concatenated_df_auroc = pd.DataFrame()
-concatenated_df_f1 = pd.DataFrame()
-concatenated_df_acc = pd.DataFrame()
-concatenated_df_precision = pd.DataFrame()
-
-# Bootstrap iterations
-for _ in tqdm.tqdm(range(1000)):
-    # Sampled data
-    indices = np.random.choice(range(len(labels)), size=len(labels), replace=True)
-    # sampled_labels = labels[indices]
-    # sampled_predicted = predicted[indices]
-    sampled_labels = labels
-    sampled_predicted = predicted
-    # Pathologies list
-    pathologies = [
-        "Medical material",
-        "Calcification",
-        "Cardiomegaly",
-        "Pericardial effusion",
-        "Coronary artery wall calcification",
-        "Hiatal hernia",
-        "Lymphadenopathy",
-        "Emphysema",
-        "Atelectasis",
-        "Lung nodule",
-        "Lung opacity",
-        "Pulmonary fibrotic sequela",
-        "Pleural effusion",
-        "Mosaic attenuation pattern",
-        "Peribronchial thickening",
-        "Consolidation",
-        "Bronchiectasis",
-        "Interlobular septal thickening",
-    ]
-
-    # Evaluate internal metrics
-    dfs_auroc = evaluate_internal(
-        sampled_predicted, sampled_labels, pathologies, data_dir
-    )
-    concatenated_df_auroc = pd.concat([concatenated_df_auroc, dfs_auroc])
-
-    # Write AUROC to Excel
-    writer = pd.ExcelWriter(
-        Path(data_dir) / "aurocs_bootstrap.xlsx", engine="xlsxwriter"
-    )
-    concatenated_df_auroc.to_excel(writer, sheet_name="Sheet1", index=False)
-    writer.close()
-
-    f1s = []
-    accs = []
-    precisions = []
-
-    # Calculate metrics for each label
+    # Find threshold for each label
     for i in range(18):
-        prob = sampled_predicted[:, i]
-        label = sampled_labels[:, i]
-        threshold = thresholds[i]
-        pred = (prob > threshold).astype(int)
+        logit = predicted[:, i]
+        l = labels[:, i]
+        prob = logit
+        threshold = find_threshold(prob, l)
+        thresholds.append(threshold)
 
-        f1 = f1_score(label, pred, average="weighted")
-        acc = accuracy_score(label, pred)
-        precision = precision_score(label, pred)
+    # Initialize DataFrames for storing evaluation metrics
+    concatenated_df_auroc = pd.DataFrame()
+    concatenated_df_f1 = pd.DataFrame()
+    concatenated_df_acc = pd.DataFrame()
+    concatenated_df_precision = pd.DataFrame()
 
-        f1s.append(f1)
-        accs.append(acc)
-        precisions.append(precision)
+    # Bootstrap iterations
+    for _ in tqdm.tqdm(range(1000)):
+        # Sampled data
+        # indices = np.random.choice(range(len(labels)), size=len(labels), replace=True)
+        # sampled_labels = labels[indices]
+        # sampled_predicted = predicted[indices]
+        sampled_labels = labels
+        sampled_predicted = predicted
+        # Pathologies list
+        pathologies = [
+            "Medical material",
+            "Calcification",
+            "Cardiomegaly",
+            "Pericardial effusion",
+            "Coronary artery wall calcification",
+            "Hiatal hernia",
+            "Lymphadenopathy",
+            "Emphysema",
+            "Atelectasis",
+            "Lung nodule",
+            "Lung opacity",
+            "Pulmonary fibrotic sequela",
+            "Pleural effusion",
+            "Mosaic attenuation pattern",
+            "Peribronchial thickening",
+            "Consolidation",
+            "Bronchiectasis",
+            "Interlobular septal thickening",
+        ]
 
-    # Store metrics in DataFrames
-    dfs_f1 = pd.DataFrame([f1s], columns=pathologies)
-    dfs_acc = pd.DataFrame([accs], columns=pathologies)
-    dfs_precision = pd.DataFrame([precisions], columns=pathologies)
+        # Evaluate internal metrics
+        dfs_auroc = evaluate_internal(
+            sampled_predicted, sampled_labels, pathologies, data_dir
+        )
+        concatenated_df_auroc = pd.concat([concatenated_df_auroc, dfs_auroc])
 
-    concatenated_df_f1 = pd.concat([concatenated_df_f1, dfs_f1])
-    concatenated_df_acc = pd.concat([concatenated_df_acc, dfs_acc])
-    concatenated_df_precision = pd.concat([concatenated_df_precision, dfs_precision])
+        # Write AUROC to Excel
+        writer = pd.ExcelWriter(
+            Path(data_dir) / "aurocs_bootstrap.xlsx", engine="xlsxwriter"
+        )
+        concatenated_df_auroc.to_excel(writer, sheet_name="Sheet1", index=False)
+        writer.close()
 
-    # Write F1, accuracy, and precision to Excel
-    writer = pd.ExcelWriter(Path(data_dir) / "f1_bootstrap.xlsx", engine="xlsxwriter")
-    concatenated_df_f1.to_excel(writer, sheet_name="Sheet1", index=False)
-    writer.close()
+        f1s = []
+        accs = []
+        precisions = []
 
-    writer = pd.ExcelWriter(Path(data_dir) / "acc_bootstrap.xlsx", engine="xlsxwriter")
-    concatenated_df_acc.to_excel(writer, sheet_name="Sheet1", index=False)
-    writer.close()
+        # Calculate metrics for each label
+        for i in range(18):
+            prob = sampled_predicted[:, i]
+            label = sampled_labels[:, i]
+            threshold = thresholds[i]
+            pred = (prob > threshold).astype(int)
 
-    writer = pd.ExcelWriter(
-        Path(data_dir) / "precision_bootstrap.xlsx", engine="xlsxwriter"
-    )
-    concatenated_df_precision.to_excel(writer, sheet_name="Sheet1", index=False)
-    writer.close()
+            f1 = f1_score(label, pred, average="weighted")
+            acc = accuracy_score(label, pred)
+            precision = precision_score(label, pred)
+
+            f1s.append(f1)
+            accs.append(acc)
+            precisions.append(precision)
+
+        # Store metrics in DataFrames
+        dfs_f1 = pd.DataFrame([f1s], columns=pathologies)
+        dfs_acc = pd.DataFrame([accs], columns=pathologies)
+        dfs_precision = pd.DataFrame([precisions], columns=pathologies)
+
+        concatenated_df_f1 = pd.concat([concatenated_df_f1, dfs_f1])
+        concatenated_df_acc = pd.concat([concatenated_df_acc, dfs_acc])
+        concatenated_df_precision = pd.concat([concatenated_df_precision, dfs_precision])
+
+        # Write F1, accuracy, and precision to Excel
+        writer = pd.ExcelWriter(data_dir / "f1_bootstrap.xlsx", engine="xlsxwriter")
+        concatenated_df_f1.to_excel(writer, sheet_name="Sheet1", index=False)
+        writer.close()
+
+        writer = pd.ExcelWriter(data_dir / "acc_bootstrap.xlsx", engine="xlsxwriter")
+        concatenated_df_acc.to_excel(writer, sheet_name="Sheet1", index=False)
+        writer.close()
+
+        writer = pd.ExcelWriter(
+            data_dir / "precision_bootstrap.xlsx", engine="xlsxwriter"
+        )
+        concatenated_df_precision.to_excel(writer, sheet_name="Sheet1", index=False)
+        writer.close()
