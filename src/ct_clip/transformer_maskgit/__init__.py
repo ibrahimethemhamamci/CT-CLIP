67ea7f4564c78779fe42bdc501d3a014c9727640
John Flavin <flavinj@gmail.com>
2024-09-10 10:16:38 -0500

    Move old package READMEs into docstrings, remove remaining old package files

--- src/ct_clip/transformer_maskgit/__init__.py
+++ src/ct_clip/transformer_maskgit/__init__.py
@@ -1,3 +1,9 @@
+"""
+This module is an adaptation of the transformer_maskgit module from
+[GenerateCT](https://github.com/ibrahimethemhamamci/GenerateCT).
+It introduces modifications to the patch embeddings of the CT-ViT model and enables the
+return of embedded layers for utilization in CT-CLIP.
+"""
 from transformer_maskgit.MaskGITTransformer import MaskGITTransformer, CTViT, MaskGit, TokenCritic, make_video
 from transformer_maskgit.videotextdataset import VideoTextDataset
 from transformer_maskgit.ctvit_trainer import CTViTTrainer
