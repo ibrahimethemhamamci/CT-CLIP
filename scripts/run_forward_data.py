0f679cb0571c0d35cc7333881417abfa7aab87f7
suprosanna <suprosanna.shit@tum.de>
2025-07-18 14:32:43 +0200

    fixed hardcoded meta path, added #TODO

--- scripts/run_forward_data.py
+++ scripts/run_forward_data.py
@@ -1,15 +1,12 @@
-import torch
 from transformer_maskgit import CTViT
 from transformers import BertTokenizer, BertModel
 from ct_clip import CTCLIP
 from forward_data import CTClipInference
-import accelerate
 
 tokenizer = BertTokenizer.from_pretrained('microsoft/BiomedVLP-CXR-BERT-specialized',do_lower_case=True)
 text_encoder = BertModel.from_pretrained("microsoft/BiomedVLP-CXR-BERT-specialized")
 
 text_encoder.resize_token_embeddings(len(tokenizer))
-
 
 image_encoder = CTViT(
     dim = 512,
@@ -36,15 +33,15 @@ clip = CTCLIP(
 
 )
 
-clip.load("path_to_pretrained_model")
+clip.load("path_to_pretrained_model") #TODO: path to the pretrained model
+
 inference = CTClipInference(
     clip,
-    data_folder = '/path_to_data_folder/valid_or_train',
-    reports_file= "path_to_validation_or_train_reports.csv",
-    labels = "path_to_validation_or_train_predicted_labels.csv",
-    batch_size = 1,
-    results_folder = "path_to_results_folder/train_or_validation/"
-    num_train_steps = 1,
+    data_folder = '/path_to_data_folder/valid_or_train', #TODO: path to the data folder
+    reports_file= "path_to_validation_or_train_reports.csv", #TODO: path to the validation or train reports
+    labels = "path_to_validation_or_train_predicted_labels.csv", #TODO: path to the validation or train predicted labels
+    meta_file = "path_to_validation_or_train_metadata.csv", #TODO: path to the validation or train metadata
+    results_folder = "path_to_results_folder/train_or_validation/" #TODO: path to the results folder
 )
 
 inference.infer()
